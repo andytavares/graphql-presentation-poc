9f3a4d0978b9f7519f1dca9796fcd38b0a101e94
Andrew Tavares <atavares@andrews-mbp.lan>
2023-11-01 21:20:54 -0400

    Initial Commit

---
+++ packages/services/products-graph/index.js
@@ -0,0 +1,27 @@
+import { ApolloServer } from '@apollo/server';
+import { buildSubgraphSchema } from '@apollo/federation';
+import { gql } from 'graphql-tag';
+import { readFileSync } from 'node:fs';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+import data from './product.data.js';
+
+const typeDefs = gql(
+  readFileSync('./packages/services/products-graph/product.graphqls', {
+    encoding: 'utf8',
+  })
+);
+
+const resolvers = {
+  Query: {
+    products: () => data,
+  },
+};
+
+const server = new ApolloServer({
+  schema: buildSubgraphSchema({ typeDefs, resolvers }),
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4002 } });
+
+console.log(`Server running at: ${url}`);
---
+++ packages/services/products-graph/product.data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    id: 1,
+    name: 'Couch',
+    description: 'Just a couch.',
+    price: 1500.0,
+    inStock: true,
+  },
+  {
+    id: 2,
+    name: 'Table',
+    description: 'Just a table.',
+    price: 500.0,
+    inStock: true,
+  },
+  {
+    id: 3,
+    name: 'Chair',
+    description: 'Just a chair.',
+    price: 100.0,
+    inStock: true,
+  },
+];
+
+export default data;
